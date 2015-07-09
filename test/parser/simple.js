42a8ab81e8a66cfb42c2eebf8030a3b3d434a8c4
Brendan Ashworth <brendan.ashworth@me.com>
2015-07-09 00:53:08 -0700

    test: DRY

--- test/parser/simple.js
+++ test/parser/simple.js
@@ -3,76 +3,43 @@
 const test = require('tape');
 const Parser = require('../../src/Parser');
 
-test('empty line', function(t) {
+function go(t, code, eq) {
     t.plan(1);
-
-    var parser = new Parser('');
-    parser.start();
-
-    t.deepEqual(parser.compiled, []);
-});
-
-test('line without opcode', function(t) {
-    t.plan(1);
-
-    var parser = new Parser('; comment starts at column 0');
-    parser.start();
-
-    t.deepEqual(parser.compiled, []);
-});
-
-test('single opcode', function(t) {
-    t.plan(1);
-
-    var parser = new Parser('iadd');
-    parser.start();
-
-    t.deepEqual(parser.compiled, [0x40]);
-});
-
-test('single opcode with whitespace', function(t) {
-    t.plan(1);
-
-    var parser = new Parser('  pop ');
-    parser.start();
-
-    t.deepEqual(parser.compiled, [0x11]);
-});
-
-test('single opcode with whitespace and comment', function(t) {
-    t.plan(1);
-
-    var parser = new Parser('  nop   ; comment');
-    parser.start();
-
-    t.deepEqual(parser.compiled, [0x01]);
-});
-
-test('single opcode with arguments', function(t) {
-    t.plan(1);
-
-    var parser = new Parser('load 0x04 12');
-    parser.start();
-
-    t.deepEqual(parser.compiled, [0x20, 0x04, 0x0c]);
-});
-
-test('single opcode with arguments and whitespace and comment', function(t) {
-    t.plan(1);
-
-    var parser = new Parser('   push    0x00    ; push something to stack');
-    parser.start();
-
-    t.deepEqual(parser.compiled, [0x10, 0x00]);
-});
-
-test('simple loop', function(t) {
-    t.plan(1);
-
-    var code = 'func: \n nop \n jump func';
 
     var parser = new Parser(code);
     parser.start();
 
-    t.deepEqual(parser.compiled, [0x01, 0x03, 0]);
+    t.deepEqual(parser.compiled, eq);
+}
+
+test('empty line', function(t) {
+    go(t, '', []);
+});
+
+test('line without opcode', function(t) {
+    go(t, '; comment starts at column 0', []);
+});
+
+test('single opcode', function(t) {
+    go(t, 'iadd', [0x40]);
+});
+
+test('single opcode with whitespace', function(t) {
+    go(t, '  pop ', [0x11]);
+});
+
+test('single opcode with whitespace and comment', function(t) {
+    go(t, '  nop   ; comment', [0x01]);
+});
+
+test('single opcode with arguments', function(t) {
+    go(t, 'load 0x04 12', [0x20, 0x04, 0x0c]);
+});
+
+test('single opcode with arguments and whitespace and comment', function(t) {
+    go(t, '   push    0x00    ; push something to stack', [0x10, 0x00]);
+});
+
+test('simple loop', function(t) {
+    go(t, 'func: \n nop \n jump func', [0x01, 0x03, 0]);
 });
